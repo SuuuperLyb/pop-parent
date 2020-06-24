6d1f47ac67381ac150ecc37cd8c33c7b6658d3ab
superlyb <2826475982@qq.com>
2020-06-24 21:34:24 +0800

    新增购物车表

---
+++ pop-common/src/main/java/com/offway/common/mapper/TShoppingCarMapper.java
@@ -0,0 +1,16 @@
+package com.offway.common.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.offway.common.entity.TShoppingCar;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author LYB
+ * @since 2020-06-24
+ */
+public interface TShoppingCarMapper extends BaseMapper<TShoppingCar> {
+
+}
--- pop-lyb/src/test/java/com/offway/poplyb/PopLybApplicationTests.java
+++ pop-lyb/src/test/java/com/offway/poplyb/PopLybApplicationTests.java
@@ -1,12 +1,9 @@
 package com.offway.poplyb;
 
-import com.offway.common.three.JedisCore;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.Resource;
 
 @SpringBootApplication
 @MapperScan("com.offway.common.mapper")
