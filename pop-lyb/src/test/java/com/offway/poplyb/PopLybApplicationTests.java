7f54985ce6f4a9f97d203548e0b36c58ecfd6b29
superlyb <2826475982@qq.com>
2020-06-24 20:27:59 +0800

    统一返回值 增加redis依赖

--- pop-lyb/src/test/java/com/offway/poplyb/PopLybApplicationTests.java
+++ pop-lyb/src/test/java/com/offway/poplyb/PopLybApplicationTests.java
@@ -1,13 +1,19 @@
 package com.offway.poplyb;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.offway.common.three.JedisCore;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class PopLybApplicationTests {
+import javax.annotation.Resource;
 
-    @Test
-    void contextLoads() {
+@SpringBootApplication
+@MapperScan("com.offway.common.mapper")
+public class PopLybApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopLybApplicationTests.class, args);
     }
 
 }
---
+++ pop-lyb/src/test/java/com/offway/poplyb/test/TestJedis.java
@@ -0,0 +1,18 @@
+package com.offway.poplyb.test;
+
+import com.offway.common.three.JedisCore;
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
+
+//public class TestJedis {
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//        JedisCore jedisCore = new JedisCore("");
+//        System.out.println(jedisCore.getVal("name"));
+//    }
+//
+//}
