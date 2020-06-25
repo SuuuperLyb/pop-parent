1db0d351910f2b4cd405d35b2e8b1d18cbd5f997
superlyb <2826475982@qq.com>
2020-06-25 14:55:29 +0800

    增加用户折扣表

---
+++ pop-common/src/main/java/com/offway/common/mapper/TUserDiscountMapper.java
@@ -0,0 +1,16 @@
+package com.offway.common.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.offway.common.entity.TUserDiscount;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author LYB
+ * @since 2020-06-25
+ */
+public interface TUserDiscountMapper extends BaseMapper<TUserDiscount> {
+
+}
--- pop-lyb/src/main/java/com/offway/Lyb/controller/MainPageController.java
+++ pop-lyb/src/main/java/com/offway/Lyb/controller/MainPageController.java
@@ -1,11 +1,49 @@
 package com.offway.Lyb.controller;
 
 
+import com.offway.Lyb.sevice.MainPageService;
+import com.offway.common.entity.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainPageController {
+
+    @Autowired
+    private MainPageService mainPageService;
+
+    //首页轮播图
+    @GetMapping("api/mainpage/findAdPic.action")
+    public R findAdPic(){
+        return mainPageService.findAdPic();
+    }
+
+    //猜你喜欢
+    @GetMapping("api/mainpage/findUserLikesGoods.action")
+    public R findUserLikesGoods(){
+        return mainPageService.findUserLikesGoods();
+    }
+
+    //今日爆款
+    @GetMapping("api/mainpage/findTodayMoreSale.action")
+    public R findTodayMoreSale(){
+        return mainPageService.findTodayMoreSale();
+    }
+
+
+
+    //商品二级分类
+    @GetMapping("api/mainpage/findSecGoodsType.action")
+    public R findSecGoodsType(){
+        return mainPageService.findSecGoodsType();
+    }
+
+    @GetMapping("api/mainpage/findGoodsBySecTypeId.action")
+    public R findGoodsBySecTypeId(Integer sectypeId){
+        return mainPageService.findGoodsBySecTypeId(sectypeId);
+    }
+
 
 
 
