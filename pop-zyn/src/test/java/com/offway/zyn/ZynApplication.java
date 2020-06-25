815c648ed97953e1bcc60e75a675e01f1203e28a
starzyn <starzyn@qq.com>
2020-06-25 10:40:02 +0800

    三

--- pop-zyn/src/test/java/com/offway/zyn/ZynApplication.java
+++ pop-zyn/src/test/java/com/offway/zyn/ZynApplication.java
@@ -3,9 +3,12 @@ package com.offway.zyn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.offway.common.mapper")
+@ComponentScan("com.offway.common.conf")
+@ComponentScan("com.offway.zyn.config")
 public class ZynApplication {
 
     public static void main(String[] args) {
