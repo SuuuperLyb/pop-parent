2ec7bf2a52cdd08bb1be4ca09b5334cbed4c18a2
superlyb <2826475982@qq.com>
2020-06-25 10:44:21 +0800

    增加三个实体类 明星style 明星照片 后台管理员详情 增加RedisKeyConfig 去掉lombook true属性

---
+++ pop-common/src/main/java/com/offway/common/entity/TStylePhotos.java
@@ -0,0 +1,72 @@
+package com.offway.common.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.time.LocalDate;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author LYB
+ * @since 2020-06-25
+ */
+public class TStylePhotos extends Model<TStylePhotos> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 图的id
+     */
+    @TableId(value = "photo_id", type = IdType.AUTO)
+    private Integer photoId;
+
+    /**
+     * 创建时间
+     */
+    private LocalDate createTime;
+
+    /**
+     * 对应的明星风格id
+     */
+    private Integer statStyleId;
+
+    public Integer getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(Integer photoId) {
+        this.photoId = photoId;
+    }
+    public LocalDate getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDate createTime) {
+        this.createTime = createTime;
+    }
+    public Integer getStatStyleId() {
+        return statStyleId;
+    }
+
+    public void setStatStyleId(Integer statStyleId) {
+        this.statStyleId = statStyleId;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.photoId;
+    }
+
+    @Override
+    public String toString() {
+        return "TStylePhotos{" +
+            "photoId=" + photoId +
+            ", createTime=" + createTime +
+            ", statStyleId=" + statStyleId +
+        "}";
+    }
+}
