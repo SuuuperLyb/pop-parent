975eb4f74057aefe8c25d7cac51392260a28abdb
starzyn <starzyn@qq.com>
2020-06-26 11:50:45 +0800

    增加了公共类中的mp配置文件，主要是用来配置分页查询的过滤器

--- pop-zyn/src/main/java/com/offway/zyn/service/impl/TStarServiceImpl.java
+++ pop-zyn/src/main/java/com/offway/zyn/service/impl/TStarServiceImpl.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.offway.common.entity.R;
+import com.offway.common.entity.TStarStyle;
+import com.offway.common.entity.TStylePhotos;
 import com.offway.common.mapper.TStarStyleMapper;
 import com.offway.common.mapper.TStylePhotosMapper;
 import com.offway.common.three.JedisCore;
@@ -32,13 +34,21 @@ public class TStarServiceImpl implements TStarService {
     TStylePhotosMapper tspm;
     @Autowired
     JedisCore jedisCore;//redis 核心类，主要用来访问缓存
+    
+    /**
+     * @Author starzyn
+     * @Description 展示明星展示页面上的热度前三的信息
+     * @Date 10:17 2020/6/26
+     * @Param []
+     * @return com.offway.common.entity.R
+     **/
     @Override
     public R getHotStyle() {
         boolean isExit = jedisCore.isExist("hotStarInfo");//判断缓存是否有明星穿搭的轮播图信息
         if(isExit){//如果缓存中存在
-//            jedisCore.del("hotStarInfo");
-//            return Rutil.Ok();
-            return Rutil.Ok(jedisCore.getVal("hotStarInfo"));
+            jedisCore.del("hotStarInfo");
+            return Rutil.Ok();
+//            return Rutil.Ok(jedisCore.getVal("hotStarInfo"));
         }else {//缓存中不存在，去数据库中查询并添加到缓存中
 //            tStarService.getMainStarInfo();
             List<TStarStyle> tss = tssm.selectList(new QueryWrapper<TStarStyle>().orderByDesc("like_num").last("limit 0,3"));
@@ -56,16 +66,25 @@ public class TStarServiceImpl implements TStarService {
         }
     }
 
+    /**
+     * @Author starzyn
+     * @Description //TODO
+     * @Date 10:17 2020/6/26
+     * @Param [starPage, pageSize]
+     * @return com.offway.common.entity.R
+     **/
     @Override
     public R listAll(int starPage,int pageSize) {
         if(starPage==1){//如果是首页，去缓存查，如果没有就加入
             boolean isExit = jedisCore.isExist("firstStarList");//判断缓存是否有明星穿搭的轮播图信息
             if(isExit){//如果缓存中存在,对于缓存来说，第一页的数据是热数据，让在缓存中，后面的分页数据可以不放在缓存中
+//                jedisCore.del("firstStarList");
+//                return Rutil.Ok();
                 return Rutil.Ok(jedisCore.getVal("firstStarList"));
             }else {//缓存中不存在，去数据库中查询并添加到缓存中
 //            tStarService.getMainStarInfo();
-                IPage<TStarStyle> firstPage = tssm.selectPage(new Page<TStarStyle>(1,pageSize),new QueryWrapper<TStarStyle>().orderByDesc("like_num"));
-                jedisCore.set("hotStarInfo",24*68*60, JSONObject.toJSONString(firstPage));
+                IPage<TStarStyle> firstPage = tssm.selectPage(new Page<>(1,pageSize),new QueryWrapper<TStarStyle>().orderByDesc("like_num"));
+                jedisCore.set("firstStarList",24*68*60, JSONObject.toJSONString(firstPage));
                 return Rutil.Ok(JSONObject.toJSONString(firstPage));
             }
         }else{//冷数据去数据库查
